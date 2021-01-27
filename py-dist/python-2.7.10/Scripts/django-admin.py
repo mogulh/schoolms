cbe8695b48bee1113657bd6a369f8746bbc852a3
Mogulh <mogulhkiti@gmail.com>
2021-01-27 18:17:27 +0300

    Initial commit

---
+++ py-dist/python-2.7.10/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\TMF LTD\Desktop\SW\django2exe-master\py-dist\python-2.7.10\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
